7cce30e481f8b68f37093a2108cc25235ea36be2
Sasha <106591445+kchaeys2@users.noreply.github.com>
2022-08-31 23:32:29 +0900

    0831

--- 03/src/functions/Slider.js
+++ 03/src/functions/Slider.js
@@ -27,15 +27,15 @@ function Slider() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/f5001d2f-6722-402b-9b2e-af6187e90ca1.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/2e9f07bd-e866-43b4-b2e3-a707ed3b779c.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/7cdc34e1-b86a-43d2-b6dd-8e4328d27356.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/a2f01b74-ada8-4c98-b955-588fb81ba994.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/2b8af685-cdd3-43c6-8b95-ea8ab5ab4507.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/c917a4b0-602d-4549-ae79-faeaa8582f80.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/2897638e-926a-4db4-b5a0-f0bbbde7c299.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/974b30f2-2512-4427-a646-92996fd24b65.jpg"/></SwiperSlide>
+        <SwiperSlide><img class="swiperimg" alt=" " src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/eb905cc6-a482-486b-b674-6add94399e81.jpg"/></SwiperSlide>
       </Swiper>
     </>
   );
